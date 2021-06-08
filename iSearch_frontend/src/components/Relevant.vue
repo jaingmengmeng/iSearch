1aa77101b77f13b9392cc41a939586c5518873f7
jaingmengmeng <1804226997@qq.com>
2021-06-08 17:55:46 +0800

    add: relevant refresh

--- iSearch_frontend/src/components/Relevant.vue
+++ iSearch_frontend/src/components/Relevant.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="relevant">
-    相关搜索
+  <div class="relevant" v-if="relevant_list.length > 0">
+    <div class="relevant-header">
+      相关搜索
+      <div class="relevant-refresh" @click="Refresh()">
+        <Icon type="ios-refresh" class="relevant-refresh-icon" />
+        换一换
+      </div>
+    </div>
     <br />
     <div
       class="relevant-list"
@@ -10,7 +16,9 @@
       )"
       :key="i"
     >
-      <div @click="updateURL(item)">{{ i + 1 }} {{ item }}</div>
+      <div @click="updateURL(item)" class="relevant-list-item">
+        {{ i + 1 + (page_num - 1) * page_size }} {{ item }}
+      </div>
     </div>
   </div>
 </template>
@@ -34,6 +42,12 @@ export default {
         }
       });
       this.$router.go(0);
+    },
+    Refresh() {
+      this.page_num =
+        (this.page_num %
+          Math.ceil(this.relevant_list.length / this.page_size)) +
+        1;
     }
   }
 };
@@ -42,22 +56,36 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .relevant {
-  color: #222;
-  font: 18px Arial, sans-serif;
-  font-weight: 400;
-  line-height: 2;
+  .relevant-header {
+    color: #222;
+    font: 18px Arial, sans-serif;
+    font-weight: 400;
+    line-height: 2;
+    .relevant-refresh {
+      float: right;
+      cursor: pointer;
+      font: 14px Arial, sans-serif;
+      color: #2440b3;
+      font-weight: 400;
+      line-height: 36px;
+      .relevant-refresh-icon {
+        font-size: 18px;
+        line-height: 2;
+      }
+    }
+  }
   .relevant-list {
     cursor: pointer;
-    max-width: 100%;
+    width: 100%;
     color: #2440b3;
     font: 18px Arial, sans-serif;
     font-weight: 400;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    vertical-align: middle;
-    display: inline-block;
-    -webkit-line-clamp: 1;
+    line-height: 2;
+    .relevant-list-item {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
   }
   .relevant-list:hover {
     text-decoration: underline;
